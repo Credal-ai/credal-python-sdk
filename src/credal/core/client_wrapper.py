6cf3641d21d93051ad2404e203031c78b0fd4756
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2025-10-01 21:14:30 +0000

    SDK regeneration

--- src/credal/core/client_wrapper.py
+++ src/credal/core/client_wrapper.py
@@ -22,10 +22,10 @@ class BaseClientWrapper:
 
     def get_headers(self) -> typing.Dict[str, str]:
         headers: typing.Dict[str, str] = {
-            "User-Agent": "credal/0.1.6",
+            "User-Agent": "credal/0.1.7",
             "X-Fern-Language": "Python",
             "X-Fern-SDK-Name": "credal",
-            "X-Fern-SDK-Version": "0.1.6",
+            "X-Fern-SDK-Version": "0.1.7",
             **(self.get_custom_headers() or {}),
         }
         headers["Authorization"] = f"Bearer {self._get_api_key()}"
