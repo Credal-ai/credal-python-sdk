fd0aa4e44d853b0a77265330487aa5c55fbfe5d8
Thomas Baker <thomas@buildwithfern.com>
2025-11-16 10:48:04 -0500

    fix

--- tests/custom/test_verify_types.py
+++ tests/custom/test_verify_types.py
@@ -1,5 +1,15 @@
 from credal import CredalApi
 
-def test_repro() -> None:
+def test_services_can_be_imported() -> None:
     client = CredalApi(api_key="")
-    client.copilots
+    assert client.copilots is not None
+    assert client.document_catalog is not None
+    assert client.document_collections is not None
+    assert client.search is not None
+    assert client.users is not None
+
+    # Assert that the number of top-level properties of the CredalApi instance is fixed
+    # This can help ensure the API surface is not unexpectedly changed
+    # If this changes, we just need to update our service assertions
+    services = {attr for attr in dir(client) if not attr.startswith("_") and not callable(getattr(client, attr, None))}
+    assert len(services) == 5
